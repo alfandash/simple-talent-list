d0295b4d08d76e655d2dda2183a6547adf1a0317
alfandash <cumiasem91@gmail.com>
2025-02-17 20:17:03 +0700

    add registery antd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
 } from "@ant-design/icons";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 const { Header, Sider, Content } = Layout;
 
 export default function RootLayout({
@@ -33,77 +34,83 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {isLoginPage ? (
-          children
-        ) : (
-          <Layout style={{ minHeight: "100vh" }}>
-            <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-              <div
-                className="logo"
-                style={{
-                  height: 32,
-                  margin: 16,
-                  color: "#fff",
-                  fontSize: "18px",
-                  textAlign: "center",
-                  lineHeight: "32px",
-                }}
+        <AntdRegistry>
+          {isLoginPage ? (
+            children
+          ) : (
+            <Layout style={{ minHeight: "100vh" }}>
+              <Sider
+                collapsible
+                collapsed={collapsed}
+                onCollapse={setCollapsed}
               >
-                {collapsed ? "E" : "Efish Talent"}
-              </div>
-              <Menu
-                theme="dark"
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                items={[
-                  {
-                    key: "1",
-                    icon: <FileTextOutlined />,
-                    label: "Talent List",
-                    onClick: () => router.push("/list"),
-                  },
-                  {
-                    key: "2",
-                    icon: <UserOutlined />,
-                    label: "Profile",
-                  },
-                ]}
-              />
-            </Sider>
-            <Layout>
-              <Header
-                style={{
-                  padding: "0 16px",
-                  background: "#fff",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Button
-                  type="text"
-                  icon={
-                    collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
-                  }
-                  onClick={() => setCollapsed(!collapsed)}
+                <div
+                  className="logo"
+                  style={{
+                    height: 32,
+                    margin: 16,
+                    color: "#fff",
+                    fontSize: "18px",
+                    textAlign: "center",
+                    lineHeight: "32px",
+                  }}
+                >
+                  {collapsed ? "E" : "Efish Talent"}
+                </div>
+                <Menu
+                  theme="dark"
+                  mode="inline"
+                  defaultSelectedKeys={["1"]}
+                  items={[
+                    {
+                      key: "1",
+                      icon: <FileTextOutlined />,
+                      label: "Talent List",
+                      onClick: () => router.push("/list"),
+                    },
+                    {
+                      key: "2",
+                      icon: <UserOutlined />,
+                      label: "Profile",
+                    },
+                  ]}
                 />
-                <Button type="primary" onClick={handleLogout}>
-                  Logout
-                </Button>
-              </Header>
-              <Content
-                style={{
-                  margin: "16px",
-                  padding: 24,
-                  background: "#fff",
-                  minHeight: 280,
-                }}
-              >
-                {children}
-              </Content>
+              </Sider>
+              <Layout>
+                <Header
+                  style={{
+                    padding: "0 16px",
+                    background: "#fff",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Button
+                    type="text"
+                    icon={
+                      collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
+                    }
+                    onClick={() => setCollapsed(!collapsed)}
+                  />
+                  <Button type="primary" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </Header>
+                <Content
+                  style={{
+                    margin: "16px",
+                    padding: 24,
+                    background: "#fff",
+                    minHeight: 280,
+                  }}
+                >
+                  {children}
+                </Content>
+              </Layout>
             </Layout>
-          </Layout>
-        )}
+          )}
+        </AntdRegistry>
       </body>
     </html>
   );
